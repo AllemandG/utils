2872c1e9f131d373ad9bb2153a9146d112a48fc9
Allemand Guillaume <allemandg.contact@gmail.com>
2017-11-24 14:52:25 +0100

    clean code

--- src/main/java/utils/Main.java
+++ src/main/java/utils/Main.java
@@ -1,12 +1,15 @@
 package utils;
 
-public class Main {
+import java.util.logging.Logger;
 
+public class Main {
+	private static Logger logger = Logger.getAnonymousLogger();
+	
 	public static void main(String[] args) {
 		HttpDataService httpData = new HttpDataService();
 		
 		MyIpService ipService = new MyIpService(httpData);
 		
-		System.out.println(ipService.getMyIp());
+		logger.info(ipService.getMyIp());
 	}
 }
