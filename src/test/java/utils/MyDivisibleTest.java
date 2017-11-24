f3dbc93142a9e7c2f0a0eef655dd2fbba214fbf5
Allemand Guillaume <allemandg.contact@gmail.com>
2017-11-24 16:04:14 +0100

    ajouts tests

--- src/test/java/utils/MyDivisibleTest.java
+++ src/test/java/utils/MyDivisibleTest.java
@@ -16,6 +16,30 @@ public class MyDivisibleTest {
 	}
 	
 	@Test
+	public void testDivisibleNegatifNonDivisible () {
+		List<Integer> list = MyDivisible.estDivisiblePar3par5OuLesDeux(-7);
+		Assert.assertThat(list, IsEqual.equalTo(Arrays.asList()));
+	}
+	
+	@Test
+	public void testDivisibleNegatifDivisiblePar3 () {
+		List<Integer> list = MyDivisible.estDivisiblePar3par5OuLesDeux(-9);
+		Assert.assertThat(list, IsEqual.equalTo(Arrays.asList(3)));
+	}
+	
+	@Test
+	public void testDivisibleNegatifDivisiblePar5 () {
+		List<Integer> list = MyDivisible.estDivisiblePar3par5OuLesDeux(-20);
+		Assert.assertThat(list, IsEqual.equalTo(Arrays.asList(5)));
+	}
+	
+	@Test
+	public void testDivisibleNegatifDivisibleParLesDeux () {
+		List<Integer> list = MyDivisible.estDivisiblePar3par5OuLesDeux(-30);
+		Assert.assertThat(list, IsEqual.equalTo(Arrays.asList(3, 5)));
+	}
+	
+	@Test
 	public void testDivisiblePar3 () {
 		List<Integer> list = MyDivisible.estDivisiblePar3par5OuLesDeux(6);
 		Assert.assertThat(list, IsEqual.equalTo(Arrays.asList(3)));
